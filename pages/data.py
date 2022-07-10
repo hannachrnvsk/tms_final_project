9b35b2f689513ceacc4c3ef8459059151ef1b678
hannacrnvsk <4ernyavskaya.aa@gmail.com>
2022-07-10 22:12:04 +0200

    RandomUsers data class added

--- pages/data.py
+++ pages/data.py
@@ -1,3 +1,7 @@
+import random
+import string
+
+
 class Product:
 
     def __init__(self, name, price ,amount,size):
@@ -19,7 +23,7 @@ class Emails:
 
 
 class Users:
-    def __init__(self, first_name=None, last_name=None, email=None, password=None,
+    def __init__(self, first_name=None, last_name=None, email= None, password=None,
                  day_of_birth=None, month_of_birth=None, year_of_birth=None,
                  company=None, address_street=None, address_build=None,
                  city=None, state=None ,postal_code=None, country=None, additional_info=None,home_phone=None,
@@ -49,9 +53,58 @@ valid_user_already_created = Users("Kate", "Romanova", "romakat@gmail.com", "qwe
                    "24","Wasrsaw", "Arizona", "45871", "United States",
                    "married", "2345678", "14256379", "romakat")
 
+user_invalid_password = Users("Gloria", "Bronson", "nonsons@mail.ru", "q",
+                   "15","6", "1996", "TeachMeSkills", "mirskogo",
+                   "24","New-York", "California", "45871", "United States",
+                   "married", "2342178", "14356379", "bronialex")
+
+user_invalid_postalcode = Users("Gloria", "Bronson", "lion@mail.ru", "q234567",
+                   "15","6", "1996", "TeachMeSkills", "mirskogo",
+                   "24","New-York", "California", "45871th8ui", "United States",
+                   "married", "2342178", "14356379", "bronialex")
+
+user_invalid_date_of_birth = Users("Gloria", "Bronson", "anson@mail.ru", "gloriab",
+                   "31","2", "1996", "TeachMeSkills", "mirskogo",
+                   "24","New-York", "California", "45871", "United States",
+                   "married", "2342178", "14356379", "bronialex")
+
+user_invalid_mobile_phone = Users("Gloria", "Bronson", "abrumio@mail.ru", "qgtytjt",
+                   "15","6", "1996", "TeachMeSkills", "mirskogo",
+                   "24","New-York", "California", "45871", "United States",
+                   "married", "2342178", "rdghynu8yh", "bronialex")
+
+user_invalid_name = Users("123456", "Bronson", "ayro@mail.ru", "qgtytjt",
+                   "15","6", "1996", "TeachMeSkills", "mirskogo",
+                   "24","New-York", "California", "45871", "United States",
+                   "married", "2342178", "14356379", "bronialex")
+
+user_invalid_homephone = Users("123456", "Bronson", "ay67o@mail.ru", "qgtytjt",
+                   "15","6", "1996", "TeachMeSkills", "mirskogo",
+                   "24","New-York", "California", "45871", "United States",
+                   "married", "djgnkj", "14356379", "bronialex")
 
 
 
+class RandomUsers:
+    def __init__(self, state=None, country=None):
+        self.first_name = ''.join(random.choice(string.ascii_lowercase) for i in range(8))
+        self.last_name = ''.join(random.choice(string.ascii_lowercase) for i in range(8))
+        self.email = self.first_name + "@gmail.com"
+        self.password = random.randint(10000, 999999)
+        self.day_of_birth = str(random.randint(1, 28))
+        self.month_of_birth = str(random.randint(1, 12))
+        self.year_of_birth = str(random.randint(1965, 2004))
+        self.company = ''.join(random.choice(string.ascii_lowercase) for i in range(8))
+        self.address_street = ''.join(random.choice(string.ascii_lowercase) for i in range(8))
+        self.address_build = random.randint(1, 999)
+        self.city = ''.join(random.choice(string.ascii_lowercase) for i in range(7))
+        self.state = state
+        self.postal_code = random.randint(10000, 99999)
+        self.country = country
+        self.additional_info = ''.join(random.choice(string.ascii_lowercase) for i in range(8))
+        self.home_phone = random.randint(10000, 999999)
+        self.mobile_phone = random.randint(100000, 9999999)
+        self.alias = ''.join(random.choice(string.ascii_lowercase) for i in range(8))
 
 
 
