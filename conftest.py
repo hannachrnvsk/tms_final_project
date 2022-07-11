9658352d4e1115c56b403477e957d39f03ba690b
hannacrnvsk <4ernyavskaya.aa@gmail.com>
2022-07-11 21:31:44 +0200

    modified conftest

--- conftest.py
+++ conftest.py
@@ -17,16 +17,19 @@ def browser(request):
     # language = request.config.getoption("language")
     print("\nstart browser for test..")
     options = Options()
+    options.binary_location = "/usr/bin/google-chrome"
     # options.add_experimental_option('prefs', {'intl.accept_languages': language})
     options.add_argument("--headless") # change to "chrome" to see
     options.add_argument("--start-maximized")
     options.add_argument("--no-sandbox")
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument("--disable-setuid-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1980,1080")
     options.add_argument(f"user-agent = {user}")
-    browser = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=options)
+    browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(10)
     yield browser
     print("\nmust quit browser..")
     browser.quit()
+
